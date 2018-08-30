910e19063278885dff45159007a6232ef655e463
Alejandro Hernandez <Alejandrehl@icloud.com>
2018-08-29 22:50:49 -0300

    recibiendo parametros por url

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html'
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  constructor( private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.params.subscribe( params => {
+      console.log(params);
+    });
+  }
 
   ngOnInit() {
   }
